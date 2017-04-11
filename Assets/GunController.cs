210906a937f069aa683e03dbd37f82618ef3aaee
Arun <rajendranarun95@gmail.com>
2017-04-11 23:36:29 +0530

    First commit

---
+++ Assets/GunController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour 
+{
+	/*public float speed;
+	public GameObject Bullet;
+	public GameObject newbullet;
+	void Start()
+	{
+		//setCountText();
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+		
+		if (Input.GetKeyDown ("space"))
+		{   
+			Fire ();
+			//newbullet=Instantiate (Bullet,transform.position,transform.rotation);
+			//newbullet.transform.position += transform.forward * Time.deltaTime * speed;
+		}
+	}
+	// Fire a bullet 
+	void Fire () {
+		// Create a new bullet pointing in the same direction as the gun 
+		newbullet=Instantiate (Bullet,transform.position,transform.rotation) as GameObject;;
+	} 
+*/
+}
---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public float bulletspeed=50;
+	public float count=0;
+
+	public void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		gameObject.transform.position += movement * bulletspeed * Time.deltaTime;
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Wall") 
+		{
+			//other.gameObject.SetActive (false);
+			Destroy(gameObject);
+		}
+		if (other.gameObject.tag == "PickUp") 
+		{
+			//other.gameObject.SetActive (false);
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+	void OnCollisionEnter(Collision thecollision){
+		if (thecollision.gameObject.tag == "Wall") 
+		{
+			//other.gameObject.SetActive (false);
+			Destroy(this.gameObject);
+			print ("Destroyed");
+		}
+	}
+}
